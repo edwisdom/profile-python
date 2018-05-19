773e790b7dd34d41c19683d1a65b724eb8502bec
Edwin Jain <Edwin.Jain@tufts.edu>
2018-05-19 03:10:45 -0400

    First commit

---
+++ func_test.py
@@ -0,0 +1,36 @@
+import time
+import matplotlib.pyplot as plt
+
+def f():
+	return
+
+def g():
+	return f()
+
+def h():
+	return g()
+
+def j():
+	return h()
+
+def time_func(func, iters):
+	times = []
+	for loops in iters:
+		start = time.time()
+		for i in iters:
+			func()
+		end = time.time()
+		times.append(end-start)
+	return times
+
+iters = range(1, 10000, 100)
+funcs = [f, g, h, j]
+
+def plot_func_times(funcs, iters):
+	for func_i in range(len(funcs)):
+		func_times = time_func(funcs[func_i], iters)
+		plt.plot(iters, func_times, label=(str(func_i) + ' level(s) of indirection'))
+	plt.legend(loc='upper right')
+
+plot_func_times(funcs, iters)
+plt.show()
---
+++ time_test.py
@@ -0,0 +1,34 @@
+import numpy as np 
+import random
+import time
+import matplotlib.pyplot as plt
+
+
+lengths = range(1000, 100001, 1000)
+a_list = []
+py_times = []
+np_times = []
+
+for length in lengths:
+	for l in range(length):
+		num = random.random()
+		a_list.append(num)
+	
+	start = time.time()
+	sum(a_list)/len(a_list)
+	end = time.time()
+	py_times.append(end-start)
+	
+	start = time.time()
+	np.mean(a_list)
+	end = time.time()
+	np_times.append(end-start)
+
+plt.figure()
+plt.plot(lengths, py_times, 'bo', label='Native Python average')
+plt.plot(lengths, np_times, 'g-', label='Numpy mean')
+plt.legend(loc='upper left')
+plt.show()
+
+
+
